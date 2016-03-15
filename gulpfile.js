56970a62eb0ea5a2a0cca0c196ab847af793af13
Michał Żaloudik <ponury.kostek@gmail.com>
2016-03-15 10:39:35 +0100

    browser module update

--- gulpfile.js
+++ gulpfile.js
@@ -8,21 +8,22 @@ var babelify = require('babelify');
 var uglify = require('gulp-uglify');
 
 gulp.task('build', () => {
-	browserify('./index.js', {debug: true}).transform(babelify, {
+	browserify('./browser.js',{ debug: true}).transform(babelify, {
 		presets: ['es2015'],
 		compact: true,
-		global: true,
-		comments: false
+		global: false,
+		comments: true
 	}).bundle()
 		.on('error', function (err) {
 			console.error(err);
 			this.emit('end');
 		})
-		.pipe(source('browser.js'))
+		.pipe(source('JsonRpc.js'))
 		.pipe(buffer())
 		.pipe(sourcemaps.init({loadMaps: true}))
 		.pipe(sourcemaps.write('./'))
-		.pipe(gulp.dest('./dist'));
+		//.pipe(uglify({mangle:false,compress:false}))
+		.pipe(gulp.dest('./browser'));
 });
 
 gulp.task('default', ['build']);
